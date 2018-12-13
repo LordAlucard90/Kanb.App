1dfcca2cb88051b0403ea15abf540f1097f8d361
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2018-12-14 00:21:23 +0100

    More green tests

--- examples/test_django_rest_framework/first_example/tests.py
+++ examples/test_django_rest_framework/first_example/tests.py
@@ -8,6 +8,7 @@ class TaskUrlTesting(APITestCase):
 
     def setUp(self):
         self.task_list_url = reverse('example:task-list')
+        self.task_detail_url = reverse('example:task-detail', kwargs={'pk': 1})
 
     def test_given_task_url_when_get_than_status_code_eq_200(self):
         response = self.client.get(self.task_list_url)
@@ -18,3 +19,23 @@ class TaskUrlTesting(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Task.objects.count(), 1)
         self.assertEqual(Task.objects.get().title, 'First task')
+
+    def test_given_task_when_delete_than_delete_it(self):
+        self.client.post(self.task_list_url, {'title': 'First task'}, format='json')
+        self.assertEqual(Task.objects.count(), 1)
+        response = self.client.delete(self.task_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Task.objects.count(), 0)
+
+    def test_given_task_when_get_details_than_return_task_data(self):
+        self.client.post(self.task_list_url, {'title': 'First task'}, format='json')
+        response = self.client.get(self.task_detail_url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'title': 'First task', 'description': ''})
+
+    def test_given_task_when_update_description_than_check_description(self):
+        self.client.post(self.task_list_url, {'title': 'First task'}, format='json')
+        response = self.client.patch(self.task_detail_url, {'description': 'This is a description'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Task.objects.get().description, 'This is a description')
+        self.assertEqual(response.data, {'title': 'First task', 'description': 'This is a description'})
--- examples/test_django_rest_framework/first_example/urls.py
+++ examples/test_django_rest_framework/first_example/urls.py
@@ -3,8 +3,6 @@ from . import views
 from django.urls import path
 
 
-# To know more about routers urls
-# https://www.django-rest-framework.org/api-guide/routers/#defaultrouter
 router = routers.DefaultRouter(trailing_slash=True)
 router.register('task', views.TaskViewSet)
 
