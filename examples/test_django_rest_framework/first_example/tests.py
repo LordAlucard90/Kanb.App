b3150230abad948493211864bea8583fd1af4b43
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2018-12-13 23:41:39 +0100

    First tests

--- examples/test_django_rest_framework/first_example/tests.py
+++ examples/test_django_rest_framework/first_example/tests.py
@@ -1,7 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Task
 
 
-class TestIsWorking(TestCase):
+class TaskUrlTesting(APITestCase):
 
-    def test_true(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.task_list_url = reverse('task-list')
+
+    def test_given_task_url_when_get_than_status_code_eq_200(self):
+        response = self.client.get(self.task_list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_given_task_data_when_post_than_create_task(self):
+        response = self.client.post(self.task_list_url, {'name': 'First task'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.get().name, 'First task')
--- examples/test_django_rest_framework/first_example/urls.py
+++ examples/test_django_rest_framework/first_example/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 
 router = routers.DefaultRouter()
 
+app_name = 'example'
+
 urlpatterns = [
+    path('task/', views.TaskViewSet.as_view(), name='task-list')
 ]
