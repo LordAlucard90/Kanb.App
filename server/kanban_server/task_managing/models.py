c3932a8b1d78de936945410c175e7d6d342a3841
Lorenzo Bugli <bugli.lorenzo@gmail.com>
2018-12-18 17:32:40 +0100

    Created status field

--- server/kanban_server/task_managing/models.py
+++ server/kanban_server/task_managing/models.py
@@ -6,4 +6,5 @@ class Task(models.Model):
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=200, default='')
     created = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=20, default='BACKLOG')
 
--- server/kanban_server/task_managing/serializers.py
+++ server/kanban_server/task_managing/serializers.py
@@ -6,4 +6,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('title', 'description')
+        fields = ('title', 'description', 'status')
